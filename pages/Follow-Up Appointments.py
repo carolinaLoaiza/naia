d5cb929bb2d6dc7983171339fe1c3df2b67bcc6e
carolinaLoaiza <carolina1605@gmail.com>
2025-08-07 14:55:20 +0100

    remove unused code

--- pages/Follow-Up Appointments.py
+++ pages/Follow-Up Appointments.py
@@ -6,36 +6,14 @@ from datetime import datetime
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 from app.AppointmentManager import AppointmentManager
 
-
-
-# def status_with_tristate(flag, date_str):
-#     date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
-#     if flag is True:
-#         return "✅ Yes"
-#     elif date_obj < today:
-#         return "❌ No"
-#     else:
-#         return "⏳"
-    
-
 if not st.session_state.get("authentication_status"):
     st.warning("Please log in first.")
     st.stop()
 
 USER_ID = st.session_state["username"]
-# history_file = f"data/history_{USER_ID}.json"
-# appointment_tracker_file = f"data/followupTracker_{USER_ID}.json"
 appointmentManager = AppointmentManager(USER_ID)
 appointments = appointmentManager.return_appointment_info()
 
-# def create_tracker_if_needed():
-#     manager = AppointmentManager()
-#     if not os.path.exists(appointment_tracker_file):
-#         return manager.create_followup_tracker_from_history(history_file, appointment_tracker_file)
-#     else:
-#         return manager.load(appointment_tracker_file)
-
-# appointments = return_appointment_info()
 
 # Configurar página
 if "page_config_set" not in st.session_state:
